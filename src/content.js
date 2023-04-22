548af03df6a407484960dedb0fdc73533513e280
kuroppe <17245737+kuroppe1819@users.noreply.github.com>
2023-04-22 15:43:34 +0900

    feat: display badge count

--- src/content.js
+++ src/content.js
@@ -1,6 +1,21 @@
+const createBadgeCountElement = ({ text }) => {
+  const badgeCountEl = document.createElement("div");
+  badgeCountEl.id = "browser-js-error-badge-count";
+  badgeCountEl.textContent = text;
+  badgeCountEl.style.paddingLeft = "2px";
+  badgeCountEl.style.paddingRight = "2px";
+  badgeCountEl.style.color = "#FFFFFF";
+  badgeCountEl.style.background = "#030712";
+  badgeCountEl.style.position = "fixed";
+  badgeCountEl.style.top = "0";
+  badgeCountEl.style.left = "0";
+  badgeCountEl.style.zIndex = "2147483647";
+  badgeCountEl.style.fontSize = "16px";
+  return badgeCountEl;
+};
+
 (async () => {
   let errorCount = 0;
-
   document.addEventListener("ErrorToExtension", async () => {
     errorCount++;
     await chrome.runtime.sendMessage({
@@ -8,6 +23,14 @@
       value: { errorCount: errorCount.toString() },
     });
 
-    document.documentElement.style.border = "4px solid #DC2626";
+    const badgeCountEl = document.getElementById(
+      "browser-js-error-badge-count"
+    );
+    if (badgeCountEl) {
+      badgeCountEl.parentNode.removeChild(badgeCountEl);
+    }
+    document.documentElement.appendChild(
+      createBadgeCountElement({ text: `❌ ${errorCount}` })
+    );
   });
 })();
